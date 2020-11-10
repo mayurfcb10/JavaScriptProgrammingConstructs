35f5ce0653ade10a6f88c9da9e9402bb5e2e8861
Mayur <mayurzope555@gmail.com>
2020-11-10 23:08:00 +0530

    [Mayur]Added: Program that takes year from command line and outputs whether it is leap year or not

---
+++ Selection_3.LeapYear.js
@@ -0,0 +1,18 @@
+let year = process.argv[2];
+console.log(year);
+
+if(year % 4 ==0) {
+    if(year % 100 == 0) {
+        if(year % 400 == 0) 
+            console.log('It\'s a leap year');
+        else 
+            console.log('Not a leap year');
+    } else {
+        console.log('It\'s a leap year');
+    }
+}else {
+    console.log('Not a Leap Year');
+}
+
+
+
